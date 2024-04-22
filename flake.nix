1f90f4190e44c2a2c166b0570f3f3774bc8a8615
Michael Gao <michael.gao@toggl.com>
2024-04-23 02:00:06 +0800

    chore(nix): Setup env

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        elixirOverlay = final: prev: {
+          elixir-ls = (prev.elixir-ls.override {
+            elixir = prev.beam.packages.erlangR24.elixir_1_15;
+          });
+        };
+
+        pkgs = import nixpkgs { inherit system; overlays = [ elixirOverlay ]; };
+      in {
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              nil
+              beam.packages.erlangR24.elixir_1_15
+              elixir_ls
+            ];
+          };
+
+          ci = pkgs.mkShell {
+            buildInputs = with pkgs; [ beam.packages.erlangR24.elixir_1_15 ];
+          };
+        };
+      }
+    );
+}
